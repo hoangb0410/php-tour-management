e8938e5e832035c4b0a588b4997c0b38892d003a
hoangadamo <hoang.caoviet@adamosoft.com>
2024-11-12 10:21:34 +0700

    init

---
+++ resources/views/tour-booking.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+@section('content')
+    <section class="booking-header mb-3">
+        <div class="container">
+            <h1>Booking Submission</h1>
+        </div>
+    </section>
+    <section class="booking-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    <hr>
+                    <section class="intro mb-5">
+                        <h3>Traveler Details</h3>
+                        <p>Information we need to confirm your tour or activity</p>
+                    </section>
+                    <section class="leader-info mb-5">
+                        <h4>Lead Traveler (Adult)</h4>
+                    </section>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-4">
+
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('scripts')
+    <script></script>
+@endpush
